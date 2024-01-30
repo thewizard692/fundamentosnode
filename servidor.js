baafbf9e707e7daf5631d948f1b44164b7ca8c43
Luis alberto Gallardo cardenas <la.gallardocardenas@ugto.mx>
2024-01-29 23:52:23 -0600

    Instalamos firebase

--- servidor.js
+++ servidor.js
@@ -1,6 +1,24 @@
 const express = require('express')
+import { initializeApp } from "firebase/app"; 
 const app = express
 const port = 5002
+
+//Conexion a base de datos en firebase
+
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCEHsjtJ94hGrRvyPGXQ0zEShpTpXMPwQs",
+  authDomain: "crud-prac-4813e.firebaseapp.com",
+  projectId: "crud-prac-4813e",
+  storageBucket: "crud-prac-4813e.appspot.com",
+  messagingSenderId: "520260990722",
+  appId: "1:520260990722:web:a3d10c6b3014f67f78f41b",
+  measurementId: "G-QFDPTYHN0V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 app.get('/', (req, res) => {
     res.send('respuesta de raiz')
